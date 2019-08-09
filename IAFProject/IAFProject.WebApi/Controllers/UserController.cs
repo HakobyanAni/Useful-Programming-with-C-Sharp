d33a94a9f5382618ddfd535c2b7ee84f3214e534
Ani Hakobian <ani.hakobian.8@gmail.com>
2019-08-09 16:00:07 +0400

    Added controllers

---
+++ IAFProject/IAFProject.WebApi/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IAFProject.WebApi.Controllers
+{
+    public class UserController : BaseIAFController
+    {
+
+    }
+}
