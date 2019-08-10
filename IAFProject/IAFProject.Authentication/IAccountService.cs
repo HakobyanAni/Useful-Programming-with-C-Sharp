04afa6ac2678b87ecb82d12a73c43a2dc713cca1
AniHakobyan <annie2016l@gmail.com>
2019-08-11 00:10:22 +0400

    Added IAccountService interface

--- IAFProject/IAFProject.Authentication/IAccountService.cs
+++ IAFProject/IAFProject.Authentication/IAccountService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using IAFProject.BLL.Models.User;
-using Microsoft.AspNetCore.Identity;
 
 namespace IAFProject.Authentication
 {
