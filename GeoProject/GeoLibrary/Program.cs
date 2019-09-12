cf51716a66c217e901f01ed4d030ec406d364d2e
HakobyanAni <annie2016l@gmail.com>
2019-09-12 22:48:23 +0400

    initial commit

---
+++ GeoProject/GeoLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace GeoLibrary
+{
+    class Program
+    {
+        public static IGeometry polygon { get; set; }
+        public static IGeometry Coord { get; set; }
+        public static double Lat { get; set; }
+        public static double Lng { get; set; }
+
+        static void Main(string[] args)
+        {
+            Lat = 34.1468658447266;
+            Lng = -118.065460205078;
+
+            bool isInPolygon = polygon.Contains(Coord);
+            Console.WriteLine(isInPolygon);
+
+
+            Console.ReadKey();
+        }
+    }
+}
