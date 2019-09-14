3ab196d3b8fe6d34686451a25ab016ca4ea0c608
HakobyanAni <annie2016l@gmail.com>
2019-09-14 11:04:24 +0400

    little commit

--- GeoProject/GeoLibrary/Program.cs
+++ GeoProject/GeoLibrary/Program.cs
@@ -6,6 +6,7 @@ namespace GeoLibrary
     {
         static void Main(string[] args)
         {
+            // GeoLibrary
             // You can use this class library to calculate the distance in miles or kilometers of any two
             // latitude/longitude points on the Earth by Harvesine formula, and also to know if the given
             // point is within the given polygon.
